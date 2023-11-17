b1961758b9febf75292b6c826aa83d9cd87b9893
devlagret <sioputra4@gmail.com>
2023-11-17 17:07:51 +0700

    Add crud

--- app/Http/Controllers/CoreClientController.php
+++ app/Http/Controllers/CoreClientController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Session;
 class CoreClientController extends Controller
 {
     public function index(CoreClientDataTable $table) {
+        Session::forget('client-data');
         return $table->render('content.CoreClient.List.index');
     }
     public function add() {
@@ -37,6 +38,49 @@ class CoreClientController extends Controller
         return redirect()->route('client.index')->with(['pesan' => 'Client gagal dimasukan','alert' => 'error']);
         }
     }
+    public function detail($client_id) {
+        $sessiondata = Session::get('client-data');
+        $data = CoreClient::find($client_id);
+        return view('content.CoreClient.Detail.index',compact('sessiondata','data'));
+    }
+    public function edit($client_id) {
+        $sessiondata = Session::get('client-data');
+        $data = CoreClient::find($client_id);
+        return view('content.CoreClient.Edit.index',compact('sessiondata','data'));
+    }
+    public function processEdit(Request $request,$client_id) {
+        $sessiondata = Session::get('client-data');
+        $data = CoreClient::find($client_id);
+        try {
+            DB::beginTransaction();
+                $data->name          =$request->name;
+                $data->address       =$request->address;
+                $data->contact_person=$request->contact_person;
+                $data->phone         =$request->phone;
+                $data->email         =$request->email;
+                $data->save();
+            DB::commit();
+            return redirect()->route('client.index')->with(['pesan' => 'Client berhasil diedit','alert' => 'success']);
+            } catch (\Exception $e) {
+            DB::rollBack();
+            report($e);
+            dd($e);
+            return redirect()->route('client.index')->with(['pesan' => 'Client gagal diedit','alert' => 'error']);
+            }
+    }
+    public function delete($client_id) {
+        try {
+            DB::beginTransaction();
+            CoreClient::find($client_id)->delete();
+            DB::commit();
+            return redirect()->route('client.index')->with(['pesan' => 'Client berhasil dihapus','alert' => 'success']);
+            } catch (\Exception $e) {
+            DB::rollBack();
+            report($e);
+            return redirect()->route('client.index')->with(['pesan' => 'Client gagal dihapus','alert' => 'error']);
+            }
+    }
+    
    public function elemenAdd(Request $request) {
    $data=Session::get('client-data');
    $data[$request->name]=$request->value;
