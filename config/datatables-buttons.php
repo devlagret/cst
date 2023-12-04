a609a122ae314b296aecf20fabaf0de0c701cde4
devlagret <sioputra4@gmail.com>
2023-12-04 17:02:59 +0700

    Update Config

--- config/datatables-buttons.php
+++ config/datatables-buttons.php
@@ -57,7 +57,7 @@ return [
      * Default html builder parameters.
      */
     'parameters'    => [
-        'dom'     => 'Bfrtip',
+        'dom'     => 'frtip',
         'order'   => [[0, 'desc']],
         'buttons' => [
             'create',
