6e626f36fb8a2261a531227733dc3d1e31115cb9
verapera <116628678+verapera@users.noreply.github.com>
2024-01-06 09:43:33 +0700

    Update Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProductSeeder::class,
             AcctInvoiceSeeder::class,
+            AcctJournalVoucherSeeder::class,
             AssetSeeder::class,
         ]);
     }
